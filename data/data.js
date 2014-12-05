f4a3181b503894bdba4878e568fe05d3ec38e104
heyanqiu <heyanqiu@meituan.com>
2014-12-05 23:23:56 +0800

    update

--- data/data.js
+++ data/data.js
@@ -7,118 +7,188 @@
 define([], function(){
 	return {	
 		"index_page": {
-			"title": "狗狗像哪个明星？",
-			"content": "我家的毛小孩长得像范冰冰~你家的狗狗长得像？",
+			"title": "测测狗狗眼中的主人",
+			"content": "别以为你在狗眼里就只是主人的身份，在狗狗的世界里，你也许只是体型更大或者更狡猾的狗。狗眼中的人到底是什么样呢？",
 			"btn_value": "开始测试"
 		},
 	    "questions": [
 		    {
-		        "content": "狗狗喜欢咬东西",
+		        "content": "在狗狗吃饭时，你突然靠近狗狗，狗狗会：",
 		        "answers": [{
 		        	"title": "A",
-		            "text": "是",
-		            "score": 10
+		            "text": "狗狗停止吃饭，看着你",
+		            "score": 0
 		        }, {
 		        	"title": "B",
-		            "text": "我家的毛小孩长得像范冰冰~你家的狗狗长得像我家的毛小孩长得像范冰冰~你家的狗狗长得像我家的毛小孩长得像范冰冰~你家的狗狗长得像",
-		            "score": 100
-		        }]
-		    }, 
-		    {
-		        "content": "问题2",
-		        "answers": [{
-		        	"title": "A",
-		            "text": "是",
-		            "score": 10
-		        }, {
-		        	"title": "B",
-		            "text": "不是",
-		            "score": 100
+		            "text": "狗狗停止吃饭，开始低吼",
+		            "score": 5
 		        }, {
 		        	"title": "C",
-		            "text": "不能确定",
-		            "score": 150
-		        }, {
-		        	"title": "D",
-		            "text": "不知道",
-		            "score": 200
+		            "text": "狗狗继续吃饭，毫不在意",
+		            "score": 4
 		        }]
 		    }, 
 		    {
-		        "content": "问题3",
+		        "content": "当狗狗趴在地上，你尝试将它翻转成肚皮朝上的姿势，这时狗狗会：",
 		        "answers": [{
 		        	"title": "A",
-		            "text": "是",
-		            "score": 10
+		            "text": "表示出不顺从，奋力跃起向上",
+		            "score": 5
 		        }, {
 		        	"title": "B",
-		            "text": "不是",
-		            "score": 100
+		            "text": "很舒适的伸个懒腰",
+		            "score": 0
+		        }, {
+		        	"title": "C",
+		            "text": "逆来顺受，但也谈不上愉快",
+		            "score": 3
 		        }]
 		    }, 
 		    {
-		        "content": "问题4",
+		        "content": "用刷牙绳与狗狗进行争抢游戏，相持不下的情况下，你让狗狗放弃，这时狗狗会：",
 		        "answers": [{
 		        	"title": "A",
-		            "text": "是",
-		            "score": 10
+		            "text": "意犹未尽，继续玩耍",
+		            "score": 5
 		        }, {
 		        	"title": "B",
-		            "text": "不是",
-		            "score": 100
+		            "text": "犹豫一下，放弃玩具",
+		            "score": 0
+		        },{
+		        	"title": "C",
+		            "text": "我家狗狗根本不喜欢这个游戏",
+		            "score": 2
 		        }]
 		    }, 
 		    {
-		        "content": "问题5",
+		        "content": "当狗狗饿了，食盆中又没有食物时，狗狗会：",
 		        "answers": [{
 		        	"title": "A",
-		            "text": "是",
-		            "score": 10
+		            "text": "隔一段时间走到你面前，惹你一下，看你没反应，就走开，过会再来",
+		            "score": 3
 		        }, {
 		        	"title": "B",
-		            "text": "不是",
-		            "score": 100
+		            "text": "将食盆拱到你面前，然后可怜巴巴的看着你",
+		            "score": 0
+		        }, {
+		        	"title": "C",
+		            "text": "自己寻找盛放狗粮的袋子，自力更生，丰衣足食",
+		            "score": 5
+		        }]
+		    }, 
+		    {
+		        "content": "当你长时间看电视/看书/上网或者专注做其他事情而没有搭理狗狗，它会：",
+		        "answers": [{
+		        	"title": "A",
+		            "text": "狗狗才顾不上你呢！早自己睡觉去了",
+		            "score": 5
+		        }, {
+		        	"title": "B",
+		            "text": "狗狗卷缩在你脚边睡觉",
+		            "score": 0
+		        }, {
+		        	"title": "C",
+		            "text": "狗狗隔一段时间就会过来惹你一下，试图引起你对它的兴趣",
+		            "score": 2
+		        }]
+		    },
+			{
+		        "content": "当屋外突然发生巨响（如放鞭炮），正在隔壁房间睡觉的狗狗会：",
+		        "answers": [{
+		        	"title": "A",
+		            "text": "跑到窗前，看看怎么回事",
+		            "score": 5
+		        }, {
+		        	"title": "B",
+		            "text": "惊慌失措，在你的安慰下仍然瑟瑟发抖",
+		            "score": 0
+		        }, {
+		        	"title": "C",
+		            "text": "惊慌失措，在你的安慰下仍然瑟瑟发抖",
+		            "score": 4
+		        }]
+		    },
+			{
+		        "content": "在家庭中发生语气激烈的争吵时，狗狗会：",
+		        "answers": [{
+		        	"title": "A",
+		            "text": "藏到角落中，瑟瑟发抖",
+		            "score": 0
+		        }, {
+		        	"title": "B",
+		            "text": "找个安静的地方睡觉",
+		            "score": 5
+		        }, {
+		        	"title": "C",
+		            "text": "试图调解矛盾，或者安慰伤心的主人",
+		            "score": 1
+		        }]
+		    },
+			{
+		        "content": "如果你对其他狗狗表示出兴趣，如抚摸，赞美等，你家的狗狗会：",
+		        "answers": [{
+		        	"title": "A",
+		            "text": "生气的吠叫，用身体把那只狗狗与你隔开",
+		            "score": 5
+		        }, {
+		        	"title": "B",
+		            "text": "没有异常反应，基本不在乎这件事",
+		            "score": 3
+		        }, {
+		        	"title": "C",
+		            "text": "虽然当时并没有太激烈的反应，但事后会对你冷淡或生闷气",
+		            "score": 0
+		        }]
+		    },
+			{
+		        "content": "当你下班回家打开门的一瞬间，狗狗会：",
+		        "answers": [{
+		        	"title": "A",
+		            "text": "飞速而欢快的迎上来，与主人亲热一下后，躺倒在地，露出肚皮",
+		            "score": 0
+		        }, {
+		        	"title": "B",
+		            "text": "飞速而欢快的迎上来，与主人亲热一下后，开始检查主人的书包",
+		            "score": 2
+		        }, {
+		        	"title": "C",
+		            "text": "慢悠悠地走出来，仿佛刚刚睡醒",
+		            "score": 5
 		        }]
 		    }
 	    ],
 	    "share": {
-	    	"url": "https://www.google.com.hk/images/srpr/logo11w.png",
-	    	"content": "快来看啊，我家的狗狗像吴亚军这个狗贼！！！！！"
+	    	"url": "https://mmbiz.qlogo.cn/mmbiz/QxhI6661iau5ibjjVdGp4LOzGMBe1iasmqnqd0HxibVn5PKnu4hZuLnpiaicV4wvfF9oK2xckhvUKz7RicyjZIOpeUiaEA/0",
+	    	"content": "好准！测测主人在狗狗心中形象？"
 	    },
 	    "result_page": {
 	    	"btn_value": "立即领取",
 	    	"tip": "为狗狗<span class=\"change-color\">免费</span>领取一份礼物",
-	    	"href": "http://www.baidu.com",
+	    	"href": "http://mp.weixin.qq.com/s?__biz=MzA5NTE0MjIxMA==&mid=210442329&idx=1&sn=4664b151ccac2b4ddb72c84730633143#rd",
 	    	"results": [
 			    {
+			        "min": 35,
+			        "max": 45,
+			        "title": "软蛋",
+			        "content": "还以为你在狗狗心目中是老大吗？不要再做白日梦了！其实狗狗早把你看做可以随便欺负的软蛋一名了。"
+			    }, 
+			    {
+			        "min": 21,
+			        "max": 34,
+			        "title": "纸老虎",
+			        "content": "你在狗狗心目中是个名副其实的纸老虎。虽然看起来很可怕，但不是不可攻破的大障碍，只要稍有机会，狗狗就会挑战你的权威。"
+			    }, 
+			    {
+			        "min": 11,
+			        "max": 20,
+			        "title": "怪兽",
+			        "content": "狗狗屈服于你的“淫威”，并不是出于对你的崇拜，而只是迫于无力抵抗。你在狗狗心目中是个无法战胜的怪兽。"
+			    }, 
+			    {
 			        "min": 0,
-			        "max": 100,
-			        "title": "刘德华",
-			        "content": "你家狗狗像刘德华"
-			    }, 
-			    {
-			        "min": 101,
-			        "max": 200,
-			        "title": "刘德华2",
-			        "content": "你家狗狗像刘德华2"
-			    }, 
-			    {
-			        "min": 201,
-			        "max": 300,
-			        "title": "刘德华3",
-			        "content": "你家狗狗像刘德华3"
-			    }, 
-			    {
-			        "min": 301,
-			        "max": 400,
-			        "title": "刘德华4",
-			        "content": "你家狗狗像刘德华4"
-			    }, 
-			    {
-			        "min": 401,
-			        "max": 550,
-			        "title": "刘德华5",
-			        "content": "你家狗狗像刘德华5"
+			        "max": 10,
+			        "title": "神",
+			        "content": "在狗狗心目中，你的头上戴着光环，它发自内心的崇拜你。心甘情愿的把自己的“狗生”全部托付给你。"
 			    }
 		    ]
 	    }
